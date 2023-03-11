e7af3750b167cb8cfb6e20b2dd866f2ae7c3b4c3
Drop15 <egor-kozlov0102@mail.ru>
2023-03-11 13:54:07 +0300

    sqlite_repository added

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -2,43 +2,135 @@
 Модуль описывает репозиторий, работающий с SQLite
 """
 
-from itertools import count
 from typing import Any
-
 from bookkeeper.repository.abstract_repository import AbstractRepository, T
+from inspect import get_annotations
 
 import sqlite3
+
 
 class SQLiteRepository(AbstractRepository[T]):
     """
     Репозиторий, работающий с SQLite.
     """
 
-    def __init__(self) -> None:
-        self._container: dict[int, T] = {}
-        self._counter = count(1)
+    db_file: str
+    table_name: str
+    cls: type
+    fields: dict[str, type]
+
+    def __init__(self, db_file: str, cls: type) -> None:
+        self.db_file = db_file
+        self.table_name = cls.__name__.lower()
+        self.fields = get_annotations(cls, eval_str=True)
+        self.fields.pop('pk')
+        self.cls = cls
+        self.create_table()
 
     def add(self, obj: T) -> int:
         if getattr(obj, 'pk', None) != 0:
             raise ValueError(f'trying to add object {obj} with filled `pk` attribute')
-        pk = next(self._counter)
-        self._container[pk] = obj
-        obj.pk = pk
-        return pk
+        names = ', '.join(self.fields.keys())
+        p = ', '.join("?" * len(self.fields))
+        values = [getattr(obj, x) for x in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'INSERT INTO {self.table_name} ({names}) VALUES ({p})',
+                values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
 
     def get(self, pk: int) -> T | None:
-        return self._container.get(pk)
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            raw_res = cur.execute(
+                f"SELECT * FROM {self.table_name} WHERE pk={pk}"
+            )
+            res = self.__parse_query_to_class(raw_res.fetchone())
+        con.close()
+        return res
 
-    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
-        if where is None:
-            return list(self._container.values())
-        return [obj for obj in self._container.values()
-                if all(getattr(obj, attr) == value for attr, value in where.items())]
+    def get_all(self, where: dict[str, Any] | None = None, subquery: str | None = None) -> list[T | None]:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            query = f"SELECT * FROM {self.table_name}"
+            if where is not None:
+                query += " WHERE " + ' AND '.join(
+                    [f"{key} = {value}"
+                     for key, value in where.items()
+                     if value is not None
+                     and isinstance(value, int) or isinstance(value, float)] +
+                    [f"{key} = '{value}'"
+                     for key, value in where.items()
+                     if value is not None
+                     and not (isinstance(value, int) or isinstance(value, float))]
+                )
+            if subquery is not None:
+                query += " " + subquery
+
+            raw_res = cur.execute(query)
+            res = raw_res.fetchall()
+        con.close()
+        out = [self.__parse_query_to_class(res[pk]) for pk in range(len(res))]
+        return out
 
     def update(self, obj: T) -> None:
         if obj.pk == 0:
             raise ValueError('attempt to update object with unknown primary key')
-        self._container[obj.pk] = obj
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            sql_query = f"UPDATE {self.table_name} SET "
+            sql_query += ','.join(
+                [f"{attr} = {val}"
+                 for attr, val in obj.__dict__.items()
+                 if attr != 'pk' and val is not None
+                 and isinstance(val, int) or isinstance(val, float)] +
+                [f"{attr} = '{val}'"
+                 for attr, val in obj.__dict__.items()
+                 if attr != 'pk' and val is not None
+                 and not (isinstance(val, int) or isinstance(val, float))]
+            )
+            sql_query += f" WHERE pk={obj.pk}"
+            cur.execute(sql_query)
+        con.close()
 
     def delete(self, pk: int) -> None:
-        self._container.pop(pk)
+        if pk == 0:
+            raise ValueError('attempt to update object with unknown primary key')
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(
+                f"DELETE FROM {self.table_name} WHERE pk={pk}"
+            )
+        con.close()
+
+    def create_table(self) -> None:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            query = f"CREATE TABLE IF NOT EXISTS {self.table_name} "
+            query += "(pk INTEGER PRIMARY KEY, "
+            query += ', '.join(list(self.fields.keys())) + ')'
+            cur.execute(query)
+        con.close()
+
+    def drop_table(self) -> None:
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute(f"DROP TABLE IF EXISTS {self.table_name}")
+        con.close()
+
+    def __parse_query_to_class(self, query: tuple[Any] | None) -> T | None:
+        if query is not None:
+            query_dict = dict(zip({"pk": int} | self.fields, query))
+            out = self.cls(**query_dict)
+        else:
+            out = None
+        return out
+
+    @classmethod
+    def repository_factory(cls, models: list[type], db_file: str | None = None) -> dict[type, type]:
+        return {model: cls(db_file, model) for model in models}
